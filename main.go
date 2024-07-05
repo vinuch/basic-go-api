efcd9ab76164f07fec49fab44296d3f40e37912a
vincent edeh <vincentedeh42@gmail.com>
2024-07-05 15:49:59 +0100

    debug

--- main.go
+++ main.go
@@ -33,20 +33,44 @@ type Response struct {
 	Greeting string `json:"greeting"`
 }
 
+// func getIP(r *http.Request) string {
+// 	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+// 	userIP := net.ParseIP(ip)
+// 	if userIP == nil {
+// 		userIP = net.ParseIP(r.Header.Get("X-Real-Ip"))
+// 	}
+// 	if userIP == nil {
+// 		userIP = net.ParseIP(r.Header.Get("X-Forwarded-For"))
+// 	}
+// 	if userIP == nil {
+// 		userIP = net.IPv4(127, 0, 0, 1) // Default to localhost
+// 	}
+// 	return userIP.String()
+// }
+
 func getIP(r *http.Request) string {
-	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	// Try to get the IP address from the X-Real-Ip header
+	ip := r.Header.Get("X-Real-Ip")
+	if ip == "" {
+		// If X-Real-Ip is empty, try the X-Forwarded-For header
+		ip = r.Header.Get("X-Forwarded-For")
+	}
+	if ip == "" {
+		// If both headers are empty, fall back to RemoteAddr
+		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+	}
+	
+	// Parse the IP address
 	userIP := net.ParseIP(ip)
+	
+	// If parsing failed or no IP was found, default to localhost
 	if userIP == nil {
-		userIP = net.ParseIP(r.Header.Get("X-Real-Ip"))
+		userIP = net.IPv4(127, 0, 0, 1)
 	}
-	if userIP == nil {
-		userIP = net.ParseIP(r.Header.Get("X-Forwarded-For"))
-	}
-	if userIP == nil {
-		userIP = net.IPv4(127, 0, 0, 1) // Default to localhost
-	}
+	
 	return userIP.String()
 }
+
 
 func getLocation(ip string) (string, string) {
 	db, err := geoip2.Open("GeoLite2-City.mmdb")
@@ -94,18 +118,18 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	clientIP := getIP(r)
 	city, _ := getLocation(clientIP)
 
-	temp, err := getTemperature(city)
-	if err != nil {
-		log.Printf("Error getting temperature: %v", err)
-		http.Error(w, "Could not fetch temperature", http.StatusInternalServerError)
-		return
-	}
+	// temp, err := getTemperature(city)
+	// if err != nil {
+	// 	log.Printf("Error getting temperature: %v", err)
+	// 	http.Error(w, "Could not fetch temperature", http.StatusInternalServerError)
+	// 	return
+	// }
 
-	greeting := fmt.Sprintf("Hello, %s!, the temperature is %.1f degrees Celsius in %s", visitorName, temp, city)
+	greeting := fmt.Sprintf("Hello, %s!, the temperature is %s! degrees Celsius in %s", visitorName, city, city)
 
 	response := Response{
 		ClientIP: clientIP,
-		// Location: city,
+		Location: city,
 		Greeting: greeting,
 	}
 
